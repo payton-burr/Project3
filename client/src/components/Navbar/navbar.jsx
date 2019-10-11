cf07b3d6688a656bca89c61b462778de09c213b1
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2019-10-10 21:20:10 -0600

    added mongoose package

---
+++ client/src/components/Navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <nav className="navbar">
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        );
+    }
+}
