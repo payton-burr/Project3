7d87e60c55d1554d6ffbc47db9053db7c793d343
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2019-10-13 12:42:36 -0600

    added scss structure

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 class Nav extends Component {
     render() {
         return(
-            <nav>
-                <ul>
-                    <li></li>
-                </ul>
-            </nav>
+            <header>
+                <h4 className='header__logo'>Arcadia</h4>
+                <nav>
+                    <ul className='nav__links'>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Games</a></li>
+                        <li><a href='#'>My Games</a></li>
+                    </ul>
+                </nav>
+                <a className='cta' href='#'><button>Contact</button></a>
+            </header>
         );
     }
 }
