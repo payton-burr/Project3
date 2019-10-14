17bd035e44d04c2f9751ee307b5be3b742e7e475
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2019-10-14 17:58:39 -0600

    changed urlencoded to false

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
