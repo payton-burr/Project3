c4bc714dea3874fda9650ac6e57fa97eaf6c1587
Payton Burr <greatpayton@gmail.com>
2019-10-23 15:35:38 -0600

    added cors and dotenv packages

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const cors = require('cors');
 const path = require("path");
+
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
